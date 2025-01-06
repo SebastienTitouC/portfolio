0a0c465c6e3d6ee973fe1a3a6e01cf3d954fb5ce
SébastienTitouC <sebastien.titou.c@gmail.com>
2025-01-06 17:11:07 +0100

    Section Career OK

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/style.css'
 import '../styles/components/header.css'
 import '../styles/components/social-networks.css'
 import '../styles/components/hero.css'
+import '../styles/components/career.css'
 import '../styles/utils.css' // Should be the last
 
 import mobileNav from './utils/mobile-nav';
