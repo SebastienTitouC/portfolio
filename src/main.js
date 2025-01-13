57221250cc8f4d24eb7201c3a2c5e63c41edc261
SébastienTitouC <sebastien.titou.c@gmail.com>
2025-01-13 16:04:04 +0100

    Add popup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '../styles/modern-normalize.css' // Should be the first
 import '../styles/fonts.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/aside.css'
 import '../styles/components/social-networks.css'
 import '../styles/components/hero.css'
 import '../styles/components/career.css'
@@ -13,10 +14,11 @@ import '../styles/utils.css' // Should be the last
 import mobileNav from './utils/mobile-nav';
 import writeSubFunction from './utils/typewriter'
 import { changeCardVisibility, addDelayToSkill } from './utils/projects'
-import changeTheme from './utils/dark-mode'
+import { changeTheme, infoThemeMode } from './utils/dark-mode'
 
 mobileNav();
 writeSubFunction();
 changeCardVisibility();
 addDelayToSkill();
 changeTheme();
+infoThemeMode();
